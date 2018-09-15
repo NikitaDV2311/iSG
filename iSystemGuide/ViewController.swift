89ec113dd60ab97cda8d76c5bca5b5174027d71f
Nikita Lazarev <nikita.lazarev23@cookiedev.com>
2018-09-15 19:19:29 +0300

    Initial commit

--- iSystemGuide/ViewController.swift
+++ iSystemGuide/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //initial commit
+        //initial commit
     }
 
     override func didReceiveMemoryWarning() {
