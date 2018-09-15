9815b78fde30e7d559da2e84519953bad96067aa
Nikita Lazarev <nikita.lazarev23@cookiedev.com>
2018-09-15 20:15:11 +0300

    added base model and controller

---
+++ iSystemGuide/Controller/BaseViewController/SGBaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SGBaseViewController.swift
+//  iSystemGuide
+//
+//  Created by Nikita Lazarev on 15.09.2018.
+//  Copyright © 2018 developersFromZp. All rights reserved.
+//
+
+import UIKit
+
+class SGBaseViewController: UIViewController {
+    
+    //MARK: - Variables -
+    
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    //MARK: - Private -
+    fileprivate func initialSetup() {
+        
+    }
+}
---
+++ iSystemGuide/Model/BaseModel/SGBaseModel.swift
@@ -0,0 +1,16 @@
+//
+//  SGBaseModel.swift
+//  iSystemGuide
+//
+//  Created by Nikita Lazarev on 15.09.2018.
+//  Copyright © 2018 developersFromZp. All rights reserved.
+//
+
+import Foundation
+
+class SGBaseModel {
+    
+    public var modelID: Int = 0
+    
+    
+}
