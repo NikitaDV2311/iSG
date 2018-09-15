b0d86aa07ffc89291a9f804426eaef54494c6377
Nikita Lazarev <nikita.lazarev23@cookiedev.com>
2018-09-15 20:08:59 +0300

    added constants file

---
+++ iSystemGuide/Constants/SGConstants.swift
@@ -0,0 +1,17 @@
+//
+//  SGConstants.swift
+//  iSystemGuide
+//
+//  Created by Nikita Lazarev on 15.09.2018.
+//  Copyright © 2018 developersFromZp. All rights reserved.
+//
+
+import Foundation
+
+struct SGConstants {
+    
+    struct TextConstants {
+        static let someTextConstant = "Some Text Constant Will Be Here"
+    }
+    
+}
